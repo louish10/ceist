3aaefb1b9350da17d3de5a97b6ba0c1ad2290220
Louis Headley <louish10@hotmail.co.uk>
2023-10-13 11:57:56 +0100

    Add basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
+
 function App() {
+  const [question, setQeustion] = useState({question: '', incorrect_answers: [], correct_answer: ""})
+  const [answer, setAnswer] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [score, setScore] = useState(0)
+  const [questionsAnswered, setQuestionsAnswered] = useState(0)
+  const [revealAnswer, setRevealAnswer] = useState(false)
+  useEffect(() => {
+    if (question.question === '') {
+      getQuestion()
+    }
+  })
+  function getQuestion() {
+    fetch("https://opentdb.com/api.php?amount=1")
+      .then(res => res.json())
+      .then(data => {
+        setRevealAnswer(false)
+        setQeustion(data.results[0])
+      }
+        )
+  }
+
+  function Question() {
+    return <div dangerouslySetInnerHTML={ {__html: question.question} }></div>
+  }
+
+  function submitAnswer(answer: string) {
+    setRevealAnswer(true)
+    if (answer == question.correct_answer) {
+      setScore(score + 1)
+    }
+    setQuestionsAnswered(questionsAnswered + 1)
+    setTimeout(getQuestion, 1500)
+  }
+
+  const incorrectAnswers = question.incorrect_answers?.map(answer => <button onClick={() => submitAnswer(answer)}>{answer}</button>)
+  const correctAnswer = <button onClick={() => submitAnswer(question.correct_answer)} className={revealAnswer ? 'button-correct' : ''}>{question.correct_answer}</button>
+  const allAnswers = incorrectAnswers.concat(correctAnswer)
+
+  const isAnswerCorrect = <div>{(answer === question.correct_answer) ? 'True': 'False'}</div>
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Ceist</h1>
+        <h2>A trivia app</h2>
+      </div>
+      <Question></Question>
+      {allAnswers}
+      {isAnswerCorrect}
+      <div>{score} of {questionsAnswered}</div>
     </div>
   );
 }
